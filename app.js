080d43e3e5cad277813f251b5a528a68a77bb786
Marcel <me@marcelcutts.com>
2015-02-16 00:59:29 +0000

    Removed items to try and get GH pages to work. Will re-add then check gitignore

--- app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Declare app level module which depends on views, and components
-angular.module('catsRightMeow', [
-    'ngRoute',
-    'catshuffle.controllers',
-    'catshuffle.services'
-])
