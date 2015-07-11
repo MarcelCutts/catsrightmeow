ffc741d99eff48eada94d9da46e1ec06e3c5bb6e
Marcel Cutts <marcel.cutts@gmail.com>
2015-07-11 23:12:29 +0100

    Added new shadow to gh-pages

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 
     // Declare app level module which depends on views, and components
     angular.module('catsRightMeow', [
-        'ngRoute',
         'catshuffle.controllers',
         'catshuffle.services'
     ]);
